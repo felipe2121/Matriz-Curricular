ba7af9b5d3ebc28189e8789791130e7aae45940e
Felipe Alves Holanda <felipe.holanda12@yahoo.com.br>
2021-03-03 14:51:56 -0300

    - commit inicial.

---
+++ src/main/java/br/felipe/desafio/matrizCurricular/MatrizCurricularApplication.java
@@ -0,0 +1,13 @@
+package br.felipe.desafio.matrizCurricular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrizCurricularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrizCurricularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/felipe/desafio/matrizCurricular/MatrizCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.felipe.desafio.matrizCurricular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrizCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
